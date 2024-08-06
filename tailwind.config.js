4b814db8ab1930779ce411d1bc0259734bacbb7d
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-08-06 12:44:30 +0900

    진짜 됐나.?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 // tailwind.config.js
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  fontFamily: {
+    sans: ["Inter", ...defaultTheme.fontFamily.sans],
+  },
 };
