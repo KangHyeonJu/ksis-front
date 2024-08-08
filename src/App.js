c50f0cfbea74918424fb9d86d501e9edf262cede
baek min gyu <msmk908@naver.com>
2024-08-08 12:08:36 +0900

    2024-08-08
    애플리케이션 열기까지 완료

--- src/App.js
+++ src/App.js
@@ -7,19 +7,30 @@ function App() {
     const url = "http://localhost:3002/download";
     const link = document.createElement("a");
     link.href = url;
-    link.download = "app.zip";
+    link.download = "App Setup 0.1.0.exe";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
+  // ksis 앱 실행 이벤트
+  const handleOpenApp = () => {
+    // 특정 URL 프로토콜 호출
+    const url = "ksis://open";
+    window.location.href = url;
+  };
+
   return (
     <div className="App">
-      <img src="/images/ksis-logo.png"></img>
+      <img src="/images/ksis-logo.png" alt="Ksis Logo"></img>
       <h1>사이트 이용을 원하시면 애플리케이션을 설치해주세요.</h1>
       <h3>이미 다운로드 하셨다면 앱을 실행해주세요.</h3>
       <button onClick={handleDownload} className="download-button">
         Download for Windows
+      </button>
+      <br></br>
+      <button onClick={handleOpenApp} className="open-app-button">
+        Open Installed App
       </button>
     </div>
   );
