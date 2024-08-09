1f8c65ec7b0c5658d86214bcdbb0c3baa58c70a6
baek min gyu <msmk908@naver.com>
2024-08-09 17:30:43 +0900

    2024-08-09
    앱 경로 레지스트리 등록 완료

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,30 @@ import NoticeBoard from "./ksis/pages/notice/NoticeBoard";
 import NoticeForm from "./ksis/pages/notice/NoticeForm";
 import { ACCOUNT_FORM } from "./constants/account_constant";
 import AccountRegForm from "./ksis/pages/account/AccountRegForm";
+import Download_app from "./ksis/pages/download-app/download_app";
 
 function App() {
   return (
-    <div className="dashboard flex">
-      <Sidebar />
-      <div className="content flex-1 p-4">
-        <Routes>
-          <Route path={ACCOUNT_FORM} element={<AccountRegForm />} />
-          <Route path={PC_INVENTORY} element={<PcList />} />
-          <Route path={PC_FORM} element={<PcForm />} />
-          <Route path="/apiboard" element={<ApiBoard />} />
-          <Route path="/apiform" element={<ApiForm />} /> {/* API 등록 */}
-          <Route path="/apiform/:apiId" element={<ApiForm />} />{" "}
-          {/* API 수정 */}
-          <Route path="/filesizeboard" element={<FileSizeBoard />} />
-          <Route path="/noticeboard" element={<NoticeBoard />} />
-          <Route path="/noticeform" element={<NoticeForm />} />
-          {/* 다른 라우트들을 추가할 수 있습니다 */}
-        </Routes>
-      </div>
+    // <div className="dashboard flex">
+    //   <Sidebar />
+    //   <div className="content flex-1 p-4">
+    //     <Routes>
+    //       <Route path={ACCOUNT_FORM} element={<AccountRegForm />} />
+    //       <Route path={PC_INVENTORY} element={<PcList />} />
+    //       <Route path={PC_FORM} element={<PcForm />} />
+    //       <Route path="/apiboard" element={<ApiBoard />} />
+    //       <Route path="/apiform" element={<ApiForm />} /> {/* API 등록 */}
+    //       <Route path="/apiform/:apiId" element={<ApiForm />} />{" "}
+    //       {/* API 수정 */}
+    //       <Route path="/filesizeboard" element={<FileSizeBoard />} />
+    //       <Route path="/noticeboard" element={<NoticeBoard />} />
+    //       <Route path="/noticeform" element={<NoticeForm />} />
+    //       {/* 다른 라우트들을 추가할 수 있습니다 */}
+    //     </Routes>
+    //   </div>
+    // </div>
+    <div>
+      <Download_app />
     </div>
   );
 }
---
+++ src/ksis/pages/download-app/download_app.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./download_app.css"; // css 파일 임포트
+
+function download_app() {
+  const handleDownload = () => {
+    // 다운로드 링크를 설정
+    const url = "http://localhost:8080/api/download";
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "ElectronReact Setup 4.6.0.exe";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  // ksis 앱 실행 이벤트
+  const handleOpenApp = () => {
+    // 특정 URL 프로토콜 호출
+    const url = "ksis://open";
+    window.location.href = url;
+  };
+
+  return (
+    <div className="App">
+      <img src="/images/ksis-logo.png" alt="Ksis Logo"></img>
+      <h1>사이트 이용을 원하시면 애플리케이션을 설치해주세요.</h1>
+      <h3>이미 다운로드 하셨다면 앱을 실행해주세요.</h3>
+      <button onClick={handleDownload} className="download-button">
+        Download for Windows
+      </button>
+      <br></br>
+      <button onClick={handleOpenApp} className="open-app-button">
+        Open Installed App
+      </button>
+    </div>
+  );
+}
+
+export default download_app;
