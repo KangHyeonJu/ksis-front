66779586b2063eea2050c1b5962646350a0e021a
BJH <42goattitude@naver.com>
2024-08-21 17:32:50 +0900

    0821_WEB_TOKEN

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Sidebar from "./ksis/components/SideBar";
-import {ACCOUNT_EDIT_FORM, ACCOUNT_FORM, ACCOUNT_LIST} from "./constants/account_constant";
+import {ACCOUNT_EDIT_FORM, ACCOUNT_FORM, ACCOUNT_LIST, TOKEN_CALLBACK} from "./constants/account_constant";
 import { PC_FORM, PC_INVENTORY } from "./constants/page_constant";
 import AccountRegForm from "./ksis/pages/account/AccountRegForm";
 import PcList from "./ksis/pages/pc/PcList";
 import PcForm from "./ksis/pages/pc/PcForm";
 import AccountList from "./ksis/pages/account/AccountList";
 import AccountEditForm from "./ksis/pages/account/AccountEditForm";
+import TokenCallback from "./ksis/pages/account/TokenCallback";
 
 function App() {
     return (
@@ -21,6 +22,7 @@ function App() {
                         <Route path={PC_FORM} element={<PcForm />} />
                         <Route path={ACCOUNT_LIST} element={<AccountList />} />
                         <Route path={ACCOUNT_EDIT_FORM} element={<AccountEditForm/>} />
+                        <Route path={TOKEN_CALLBACK} element={<TokenCallback/>} />
                         {/* 다른 라우트들을 추가할 수 있습니다 */}
                     </Routes>
                 </div>
--- src/constants/account_constant.js
+++ src/constants/account_constant.js
@@ -3,3 +3,5 @@ export const ACCOUNT_FORM = "/account"
 export const ACCOUNT_LIST = "/accountList"
 
 export const ACCOUNT_EDIT_FORM = "/account/:accountId"
+
+export const TOKEN_CALLBACK = "/get-token"
