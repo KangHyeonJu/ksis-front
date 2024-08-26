e78bd9534d50963ea769d841d4c13f9efe4446e0
BJH <42goattitude@naver.com>
2024-08-26 17:25:30 +0900

    0826_ 토큰갱신 진행중

--- src/fetcher.js
+++ src/fetcher.js
@@ -9,4 +9,28 @@ const fetcher = axios.create({
   },
 });
 
+// 요청 인터셉터 설정
+fetcher.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('accessToken'); // 로컬스토리지에서 액세스 토큰 가져오기
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`; // 요청 헤더에 토큰 추가
+      }
+      return config;
+    },
+    (error) =>  Promise.reject(error)
+);
+
+// 응답 인터셉터 설정 (옵션)
+fetcher.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (error) {
+        // 401 에러 처리 로직 추가 가능 (예: 토큰 갱신)
+          console.log("에러났습니다.");
+      }
+      return Promise.reject(error);
+    }
+);
+
 export default fetcher;
