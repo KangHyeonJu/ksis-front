630c93ff9ae76a7f381611d2bebb15f3b7cd9206
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-08-26 17:44:26 +0900

    .

--- src/ksis/pages/fileMng/FileBoardModal.jsx
+++ src/ksis/pages/fileMng/FileBoardModal.jsx
@@ -3,7 +3,7 @@ import { Dialog } from "../../css/dialog";
 import { FILE_BASIC } from "../../../constants/api_constant";
 import axios from "axios";
 
-const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {  
+const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {
   const [modalData, setModalData] = useState([]); // 모달 데이터를 관리하기 위한 상태
 
   const loadModal = useCallback(async () => {
@@ -12,7 +12,7 @@ const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {
       setModalData([response.data]); // 서버에서 받은 데이터를 상태에 저장
       console.log("이미지 모달 데이터 : ", response.data);
     } catch (error) {
-      console.error('에러:', error);
+      console.error("에러:", error);
     }
   }, [originalResourceId]);
 
@@ -38,27 +38,39 @@ const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {
           <div className="p-2">
             {modalData.map((data, index) => (
               <div key={index} className="mb-4">
-                <img 
-                  src={data.filePath} 
-                  alt={data.fileTitle} 
-                  className="w-full h-auto mt-4 cursor-pointer" 
+                <img
+                  src={data.filePath}
+                  alt={data.fileTitle}
+                  className="w-full h-auto mt-4 cursor-pointer"
                 />
                 {/* 데이터 테이블 */}
                 <table className="min-w-full divide-y divide-gray-300 border-collapse border border-gray-300 mb-4 text-center">
                   <thead>
                     <tr>
                       <td className="border border-gray-300 bg-white">제목</td>
-                      <td className="border border-gray-300 bg-white">인코딩 일자</td>
-                      <td className="border border-gray-300 bg-white">해상도</td>
+                      <td className="border border-gray-300 bg-white">
+                        인코딩 일자
+                      </td>
+                      <td className="border border-gray-300 bg-white">
+                        해상도
+                      </td>
                       <td className="border border-gray-300 bg-white">포맷</td>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td className="border border-gray-300 bg-white">{data.fileTitle}</td>
-                      <td className="border border-gray-300 bg-white">{data.encodingDate}</td>
-                      <td className="border border-gray-300 bg-white">{data.resolution}</td>
-                      <td className="border border-gray-300 bg-white">{data.format}</td>
+                      <td className="border border-gray-300 bg-white">
+                        {data.fileTitle}
+                      </td>
+                      <td className="border border-gray-300 bg-white">
+                        {data.regTime}
+                      </td>
+                      <td className="border border-gray-300 bg-white">
+                        {data.resolution}
+                      </td>
+                      <td className="border border-gray-300 bg-white">
+                        {data.format}
+                      </td>
                     </tr>
                   </tbody>
                 </table>
