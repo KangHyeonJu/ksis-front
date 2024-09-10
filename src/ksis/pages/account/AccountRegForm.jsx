ce4a95cb5aac9f8c69574f4d919af6cf71eb5545
BJH <42goattitude@naver.com>
2024-09-10 14:57:20 +0900

    0910_계정등록수정조건추가

--- src/ksis/pages/account/AccountRegForm.jsx
+++ src/ksis/pages/account/AccountRegForm.jsx
@@ -212,6 +212,7 @@ const AccountRegForm = () => {
                             value={formData.businessTel}
                             onChange={handleChange}
                             required
+                            pattern="\d{3}-\d{4}-\d{4}"
                             className="bg-[#ffe69c] block w-80 ml-2 rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                     </div>
@@ -225,6 +226,7 @@ const AccountRegForm = () => {
                             type="tel"
                             value={formData.emergencyTel}
                             onChange={handleChange}
+                            pattern="\d{3}-\d{4}-\d{4}"
                             className="bg-[#ffe69c] block w-80 ml-2 rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                     </div>
