35f5c288ed52ea5a39afb9dc3a677013e036cf13
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-09-10 18:53:52 +0900

    .

--- src/ksis/pages/download-app/download_app.jsx
+++ src/ksis/pages/download-app/download_app.jsx
@@ -4,7 +4,7 @@ import "./download_app.css"; // css 파일 임포트
 function download_app() {
   const handleDownload = () => {
     // 다운로드 링크를 설정
-    const url = "http://localhost:8080/api/download";
+    const url = "http://172.20.20.254:8080/api/download";
     const link = document.createElement("a");
     link.href = url;
     link.download = "ElectronReact Setup 4.6.0.exe";
