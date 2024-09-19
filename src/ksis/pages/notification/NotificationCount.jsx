a3be0b0cd6b79b0ce5d9b2cb5060e6c5a693469e
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-09-19 20:32:15 +0900

    경로

--- src/ksis/pages/notification/NotificationCount.jsx
+++ src/ksis/pages/notification/NotificationCount.jsx
@@ -22,8 +22,8 @@ const NotificationCountComponent = () => {
   useEffect(() => {
     const token = localStorage.getItem("accessToken"); // 로컬 스토리지에서 토큰 가져오기
     const eventSource = new EventSourcePolyfill(
-      "http://localhost:8080/sse/notifications",
-      // API_BASE_URL + "/api/sse/notifications",
+      // "http://localhost:8080/sse/notifications",
+      API_BASE_URL + "/api/sse/notifications",
       {
         headers: {
           Authorization: `Bearer ${token}`, // 토큰을 헤더에 추가
