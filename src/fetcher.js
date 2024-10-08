aa02340879a66631355ea0c0447982b5aea1a01b
BJH <42goattitude@naver.com>
2024-10-08 14:05:48 +0900

    1007_fetcher수정+

--- src/fetcher.js
+++ src/fetcher.js
@@ -8,10 +8,11 @@ const fetcher = axios.create({
   headers: {},
 });
 
+const token = localStorage.getItem("accessToken"); // 로컬스토리지에서 액세스 토큰 가져오기
+
 // 요청 인터셉터 설정
 fetcher.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("accessToken"); // 로컬스토리지에서 액세스 토큰 가져오기
     if (token) {
       config.headers.Authorization = `Bearer ${token}`; // 요청 헤더에 토큰 추가
     }
@@ -25,15 +26,18 @@ fetcher.interceptors.response.use(
   (response) => response,
   async (error) => {
     if (error.response.status === 403) {
-      const accessToken = localStorage.getItem("accessToken"); // 만료된 액세스 토큰 담기
-      if (accessToken) {
         // 만료된 액세스 토큰 갱신 요청
-        const response = await fetcher.post(`${TOKEN_CALLBACK}`, null, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`, // 토큰을 Authorization 헤더에 담기
-          },
-        });
-
+          let response;
+          try {
+              response = await fetcher.post(`${TOKEN_CALLBACK}`, null, {
+                  headers: {
+                      Authorization: `Bearer ${token}`, // 토큰을 Authorization 헤더에 담기
+                  },
+              });
+          }catch(err){
+              localStorage.removeItem("accessToken");
+              return Promise.resolve();
+          }
         // 리프레시 토큰이 만료된 경우
         if (!response.data) {
           localStorage.removeItem("accessToken");
@@ -51,7 +55,6 @@ fetcher.interceptors.response.use(
         // 갱신된 액세스 토큰으로 재요청
         error.config.headers.Authorization = `Bearer ${newAccessToken}`;
         return axios(error.config);
-      }
     }
     return Promise.reject(error);
   }
