0508e664d4104684e6054dc5b44b232254b7e370
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-10-17 10:20:37 +0900

    이미지 및 영상 휴지통 완성

--- src/ksis/pages/trash/TrashImageFileBoard.jsx
+++ src/ksis/pages/trash/TrashImageFileBoard.jsx
@@ -1,15 +1,14 @@
     import React, { useState, useEffect } from "react";
-    import { FaSearch, FaEdit } from "react-icons/fa";
+    import { FaSearch } from "react-icons/fa";
     import { Link, useNavigate } from "react-router-dom";
     import ReactPaginate from "react-paginate";
     import {
-      IMAGE_ENCODING,
       TRASH_IMAGE_FILE,
       TRASH_VIDEO_FILE
     } from "../../../constants/page_constant";
     import {
         DEACTIVE_IMAGE_BOARD,
-      FILE_ORIGINAL_BASIC,
+        FILE_ACTIVE,
     } from "../../../constants/api_constant";
     import { format, parseISO } from "date-fns";
     import fetcher from "../../../fetcher";
@@ -23,8 +22,6 @@
       const postsPerPage = 16;
       const [images, setImages] = useState([]);
       const [filteredPosts, setFilteredPosts] = useState([]);
-      const [editingTitleIndex, setEditingTitleIndex] = useState(null);
-      const [newTitle, setNewTitle] = useState("");
       const navigate = useNavigate();
     
     
@@ -33,6 +30,7 @@
           .get(DEACTIVE_IMAGE_BOARD)
           .then((response) => {
             setImages(response.data);
+            setFilteredPosts(response.data);;
           })
           .catch((error) => {
             console.error("Error fetching images:", error);
@@ -45,47 +43,210 @@
         navigate(newIsOriginal ? TRASH_VIDEO_FILE : TRASH_IMAGE_FILE);
       };
 
+      const handleActivation = async (id) => {
+        if (window.confirm("정말로 이 이미지를 활성화하시겠습니까?")) {
+          try {
+            await fetcher.post(`${FILE_ACTIVE}/${id}`);
+            setImages(images.filter((image) => image.originalResourceId !== id));
+            window.alert("이미지를 활성화하였습니다.");
+          } catch (err) {
+            console.error("이미지 활성화 오류:", err);
+            window.alert("이미지 활성화에 실패했습니다.");
+          }
+        }
+      };
+
+      const handlePageChange = ({ selected }) => {
+        setCurrentPage(selected);
+      };
+      const currentPosts = filteredPosts.slice(
+        currentPage * postsPerPage,
+        (currentPage + 1) * postsPerPage
+      );
+    
+      const formatDate = (dateString) => {
+        try {
+          const date = parseISO(dateString);
+          return format(date, "yyyy-MM-dd");
+        } catch (error) {
+          console.error("Invalid date format:", dateString);
+          return "Invalid date";
+        }
+      };
       
     
       return (
         <div className="p-6">
-          <header className="mb-6">
-            <h1 className="text-4xl font-bold leading-tight tracking-tight text-gray-900 my-4">
-              비활성화 이미지 페이지
-            </h1>
-          </header>
-    
-    
-          {/* 토글 버튼 */}
-          <div className="flex justify-end space-x-2">
-            <button
-              type="button"
-              onClick={handleToggle}
-              className={`relative inline-flex items-center h-8 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 ${
-                isOriginal ? "bg-[#f39704]" : "bg-gray-200"
+        <header className="mb-6">
+          <h1 className="text-4xl font-bold leading-tight tracking-tight text-gray-900 my-4">
+            비활성화 이미지 페이지
+          </h1>
+        </header>
+  
+  {/* 검색바 입력창 */}
+  <div className="flex items-center relative flex-grow mb-4">
+          <select
+            value={searchCategory}
+            onChange={(e) => setSearchCategory(e.target.value)}
+             className="p-2 mr-2 rounded-md bg-[#f39704] text-white"
+          >
+            <option value="total">전체</option>
+            <option value="title">제목</option>
+            <option value="regDate">등록일</option>
+          </select>
+          <div className="relative flex-grow">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="검색어를 입력하세요"
+              className="w-full p-2 pl-10 border border-gray-300 rounded-md"
+            />
+            <FaSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500" />
+          </div>
+        </div>
+  
+        <div className="flex items-center justify-between mb-4">
+        
+          {/* 파일등록 버튼 */}
+        <div className="flex justify-start space-x-2 ">
+        <Link to="ksis://open">
+          <button
+            type="button"
+            className="relative inline-flex items-center rounded-md bg-[#ffcf8f] px-3 py-2 text-sm 
+            font-semibold text-black shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 
+            focus-visible:outline-offset-2 focus-visible:outline-orange-600">
+           파일 등록
+          </button>
+          </Link>
+        </div>
+  
+        {/* 토글 버튼 */}
+        <div className="flex justify-end space-x-2">
+          <button
+            type="button"
+            onClick={handleToggle}
+            className={`relative inline-flex items-center h-8 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 ${
+              isOriginal ? "bg-[#f39704]" : "bg-gray-200"
+            }`}
+            role="switch"
+            aria-checked={isOriginal}
+          >
+            <span className="sr-only">{isOriginal ? "이미지" : "영상"}</span>
+            <span
+              className={`inline-block h-7 w-7 transform rounded-full bg-white shadow ring-0 transition-transform duration-200 ease-in-out ${
+                isOriginal ? "translate-x-10" : "translate-x-0"
               }`}
-              role="switch"
-              aria-checked={isOriginal}
+              aria-hidden="true"
+            />
+            <span
+              className={`absolute left-2 right-2 text-sm font-medium text-black transition-transform duration-200 ease-in-out ${
+                isOriginal ? "text-left" : "text-right"
+              }`}
             >
-              <span className="sr-only">{isOriginal ? "이미지" : "영상"}</span>
-              <span
-                className={`inline-block h-7 w-7 transform rounded-full bg-white shadow ring-0 transition-transform duration-200 ease-in-out ${
-                  isOriginal ? "translate-x-10" : "translate-x-0"
-                }`}
-                aria-hidden="true"
-              />
-              <span
-                className={`absolute left-2 right-2 text-sm font-medium text-black transition-transform duration-200 ease-in-out ${
-                  isOriginal ? "text-left" : "text-right"
-                }`}
-              >
-                {isOriginal ? "이미지" : "영상"}
-              </span>
-            </button>
-          </div>
-    
-          </div>
-      );
-    };
+              {isOriginal ? "이미지" : "영상"}
+            </span>
+          </button>
+        </div>
+        </div>
+  
+         {/* 그리드 시작 */}
+         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 gap-4">
+          {currentPosts.length > 0 ? (
+            currentPosts.map((post, index) => (
+              <div key={index} className="grid p-1">
+                
+              {/* 카드 */}
+              <div className="rounded-lg bg-[#ffe69c] px-3 py-5 flex flex-col items-center 
+                h-full overflow-hidden max-w-xs"> {/* max-w-xs로 카드 너비 제한 */}
+                   
+                     {/* 이미지 */}
+                     <div>
+                     <div className="w-full h-full mb-1 overflow-hidden">
+                      <img
+                        src={post.thumbFilePath}
+                        alt={post.fileTitle}
+                       className="w-60 h-60 cursor-pointer object-cover object-center"
+                      />
+                      </div>
+                      </div>
+  
+                  {/* 제목 */}
+                  <div className="flex justify-between w-full">
+                      
+                  <h2 className="text-xl font-bold truncate max-w-full mx-auto justify-start" title={post.fileTitle}>
+                  {post.fileTitle}
+                  </h2>
+                        
+                        
+                       
+                     
+                  </div>
+  
+                   {/* 등록일 */}
+                   <div className="">
+                  <p className="text-gray-700 mb-2">{formatDate(post.regTime)}</p>
+                  </div>
+               
+  
+                  {/* 삭제 버튼 */}
+                  <div>
+                  <div className="items-center text-center row mx-auto p-2">
+                    <button
+                    type="button"
+                    className="mr-2 rounded-md bg-[#6dd7e5]
+                                        px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-sky-400 
+                                         focus-visible:outline-blue-600"
+                    onClick={() => handleActivation(post.originalResourceId)}
+                  >
+                    활성화
+                  </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="col-span-full text-center text-gray-500">
+              게시된 파일이 없습니다.
+            </div>
+          )}
+        </div>
+  
+        {/* 페이지네이션 */}
+        {filteredPosts.length > postsPerPage && (
+          <ReactPaginate
+            previousLabel={"이전"}
+            nextLabel={"다음"}
+            breakLabel={"..."}
+            pageCount={Math.ceil(filteredPosts.length / postsPerPage)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageChange}
+            containerClassName={"flex justify-center mt-4"}
+            pageClassName={"mx-1"}
+            pageLinkClassName={
+              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+            }
+            previousClassName={"mx-1"}
+            previousLinkClassName={
+              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+            }
+            nextClassName={"mx-1"}
+            nextLinkClassName={
+              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+            }
+            breakClassName={"mx-1"}
+            breakLinkClassName={
+              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+            }
+            activeClassName={"bg-blue-500 text-white"}
+          />
+        )}
+  
+       
+      </div>
+    );
+  };
     
 export default TrashImageFileBoard;
--- src/ksis/pages/trash/TrashVideoFileBoard.jsx
+++ src/ksis/pages/trash/TrashVideoFileBoard.jsx
@@ -11,7 +11,7 @@
   } from "../../../constants/page_constant";
   import {
     DEACTIVE_VIDEO_BOARD,
-    FILE_ENCODED_BASIC,
+    FILE_ACTIVE,
   } from "../../../constants/api_constant";
   
   const TrashVideoFileBoard = () => {
@@ -24,10 +24,6 @@
   
     const postsPerPage = 16; // 페이지당 게시물 수
     const [filteredPosts, setFilteredPosts] = useState([]); // 필터링된 게시물을 상태로 관리
-    const [editingTitleIndex, setEditingTitleIndex] = useState(null);
-    const [newTitle, setNewTitle] = useState("");
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedVideo, setSelectedVideo] = useState("");
     const navigate = useNavigate(); // Initialize useNavigate
   
     useEffect(() => {
@@ -47,25 +43,20 @@
       setIsOriginal(newIsOriginal);
       navigate(newIsOriginal ? TRASH_VIDEO_FILE : TRASH_IMAGE_FILE);
     };
-  
-    const handleEditClick = (index, title) => {
-      setEditingTitleIndex(index);
-      setNewTitle(title);
-    };
-  
+
     const handlePageChange = ({ selected }) => {
       setCurrentPage(selected);
     };
   
-    const handleDelete = async (id) => {
-      if (window.confirm("정말로 이 영상을 삭제하시겠습니까?")) {
+    const handleActivation = async (id) => {
+      if (window.confirm("정말로 이 영상을 활성화하시겠습니까?")) {
         try {
-          await fetcher.delete(FILE_ENCODED_BASIC + `/${id}`);
-          setVideos(videos.filter((video) => video.id !== id));
-          window.alert("영상을 삭제하였습니다.");
+          await fetcher.post(`${FILE_ACTIVE}/${id}`);
+          setVideos(videos.filter((image) => image.originalResourceId !== id));
+          window.alert("영상을 활성화하였습니다.");
         } catch (err) {
-          console.error("영상 삭제 오류:", err);
-          window.alert("영상 삭제에 실패했습니다.");
+          console.error("영상 활성화 오류:", err);
+          window.alert("영상 활성화에 실패했습니다.");
         }
       }
     };
@@ -80,16 +71,6 @@
       }
     };
   
-    const openResourceModal = (src) => {
-      setSelectedVideo(src);
-      setIsOpen(true);
-    };
-  
-    const closeModal = () => {
-      setIsOpen(false);
-      setSelectedVideo("");
-    };
-  
     const currentPosts = filteredPosts.slice(
       currentPage * postsPerPage,
       (currentPage + 1) * postsPerPage
@@ -97,208 +78,176 @@
   
     return (
       <div className="p-6">
-        <header className="mb-6">
-          <h1 className="text-4xl font-bold leading-tight tracking-tight text-gray-900 my-4">
-            영상 인코딩 페이지
-          </h1>
-        </header>
-  
-  {/* 검색바 입력창 */}
-  <div className="flex items-center relative flex-grow mb-4">
-          <select
-            value={searchCategory}
-            onChange={(e) => setSearchCategory(e.target.value)}
-             className="p-2 mr-2 rounded-md bg-[#f39704] text-white"
-          >
-            <option value="total">전체</option>
-            <option value="title">제목</option>
-            <option value="regDate">등록일</option>
-          </select>
-          <div className="relative flex-grow">
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="검색어를 입력하세요"
-              className="w-full p-2 pl-10 border border-gray-300 rounded-md"
-            />
-            <FaSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500" />
-          </div>
+      <header className="mb-6">
+        <h1 className="text-4xl font-bold leading-tight tracking-tight text-gray-900 my-4">
+          비활성화 영상 페이지
+        </h1>
+      </header>
+
+{/* 검색바 입력창 */}
+<div className="flex items-center relative flex-grow mb-4">
+        <select
+          value={searchCategory}
+          onChange={(e) => setSearchCategory(e.target.value)}
+           className="p-2 mr-2 rounded-md bg-[#f39704] text-white"
+        >
+          <option value="total">전체</option>
+          <option value="title">제목</option>
+          <option value="regDate">등록일</option>
+        </select>
+        <div className="relative flex-grow">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="검색어를 입력하세요"
+            className="w-full p-2 pl-10 border border-gray-300 rounded-md"
+          />
+          <FaSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500" />
         </div>
-  
-        <div className="flex items-center justify-between mb-4">
-        
-          {/* 파일등록 버튼 */}
-        <div className="flex justify-start space-x-2 ">
-        <Link to="ksis://open">
-          <button
-            type="button"
-            className="relative inline-flex items-center rounded-md bg-[#ffcf8f] px-3 py-2 text-sm 
-            font-semibold text-black shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 
-            focus-visible:outline-offset-2 focus-visible:outline-orange-600">
-           파일 등록
-          </button>
-          </Link>
-        </div>
-  
-        {/* 토글 버튼 */}
-        <div className="flex justify-end space-x-2">
-          <button
-            type="button"
-            onClick={handleToggle}
-            className={`relative inline-flex items-center h-8 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 ${
-              isOriginal ? "bg-[#f39704]" : "bg-gray-200"
+      </div>
+
+      <div className="flex items-center justify-between mb-4">
+      
+        {/* 파일등록 버튼 */}
+      <div className="flex justify-start space-x-2 ">
+      <Link to="ksis://open">
+        <button
+          type="button"
+          className="relative inline-flex items-center rounded-md bg-[#ffcf8f] px-3 py-2 text-sm 
+          font-semibold text-black shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 
+          focus-visible:outline-offset-2 focus-visible:outline-orange-600">
+         파일 등록
+        </button>
+        </Link>
+      </div>
+
+      {/* 토글 버튼 */}
+      <div className="flex justify-end space-x-2">
+        <button
+          type="button"
+          onClick={handleToggle}
+          className={`relative inline-flex items-center h-8 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 ${
+            isOriginal ? "bg-[#f39704]" : "bg-gray-200"
+          }`}
+          role="switch"
+          aria-checked={isOriginal}
+        >
+          <span className="sr-only">{isOriginal ? "이미지" : "영상"}</span>
+          <span
+            className={`inline-block h-7 w-7 transform rounded-full bg-white shadow ring-0 transition-transform duration-200 ease-in-out ${
+              isOriginal ? "translate-x-10" : "translate-x-0"
             }`}
-            role="switch"
-            aria-checked={isOriginal}
+            aria-hidden="true"
+          />
+          <span
+            className={`absolute left-2 right-2 text-sm font-medium text-black transition-transform duration-200 ease-in-out ${
+              isOriginal ? "text-left" : "text-right"
+            }`}
           >
-            <span className="sr-only">{isOriginal ? "이미지" : "영상"}</span>
-            <span
-              className={`inline-block h-7 w-7 transform rounded-full bg-white shadow ring-0 transition-transform duration-200 ease-in-out ${
-                isOriginal ? "translate-x-10" : "translate-x-0"
-              }`}
-              aria-hidden="true"
-            />
-            <span
-              className={`absolute left-2 right-2 text-sm font-medium text-black transition-transform duration-200 ease-in-out ${
-                isOriginal ? "text-left" : "text-right"
-              }`}
-            >
-              {isOriginal ? "이미지" : "영상"}
-            </span>
-          </button>
-        </div>
-        </div>
-  
-         {/* 그리드 시작 */}
-         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 gap-4">
-          {currentPosts.length > 0 ? (
-            currentPosts.map((post, index) => (
-              <div key={index} className="grid p-1">
-                
-              {/* 카드 */}
-              <div className="rounded-lg bg-[#ffe69c] px-3 py-5 flex flex-col items-center 
-                h-full overflow-hidden max-w-xs"> {/* max-w-xs로 카드 너비 제한 */}
-                     {/* 영상 */}
-                     <div>
-                     <div className="w-full h-full mb-1 overflow-hidden">
-                      <img
-                        src={post.thumbFilePath}
-                        alt={post.fileTitle}
-                       className="w-60 h-60 cursor-pointer object-cover object-center"
-                        onClick={() => openResourceModal(post.filePath)}
-                      />
-                      </div>
-                      </div>
-  
-                  {/* 제목 */}
-                  <div className="flex justify-between w-full">
-                      {editingTitleIndex === index ? (
-                        <input
-                          type="text"
-                          value={newTitle}
-                          onChange={(e) => setNewTitle(e.target.value)}
-                          className="w-full text-xl font-midium border-b text-center
-                          border-gray-400 outline-none transition-colors duration-200 
-                          focus:border-gray-600 max-w-full mx-auto justify-start"
-                      placeholder="제목을 입력해주세요." />
-                      
-                      ) : (
-                        <h2 className="text-xl font-bold truncate max-w-full mx-auto justify-start" title={post.fileTitle}>
-                        {post.fileTitle}
-                      </h2>
-                        )}
-                        <div>
-                        
-                      </div>
-                     </div>
-  
-                   {/* 등록일 */}
-                   <div className="">
-                  <p className="text-gray-700 mb-2">{formatDate(post.regTime)}</p>
-                  </div>
-               
-  
-                  {/* 삭제 버튼 */}
-                  <div>
-                  <div className="items-center text-center row mx-auto p-2">
-                    <button
-                    type="button"
-                    className="mr-2 rounded-md bg-[#f48f8f] px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-red-400 focus-visible:outline-red-600"
-                    onClick={() => handleDelete(post.encodedResourceId)}
-                  >
-                    삭제
-                  </button>
+            {isOriginal ? "이미지" : "영상"}
+          </span>
+        </button>
+      </div>
+      </div>
+
+       {/* 그리드 시작 */}
+       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 gap-4">
+        {currentPosts.length > 0 ? (
+          currentPosts.map((post, index) => (
+            <div key={index} className="grid p-1">
+              
+            {/* 카드 */}
+            <div className="rounded-lg bg-[#ffe69c] px-3 py-5 flex flex-col items-center 
+              h-full overflow-hidden max-w-xs"> {/* max-w-xs로 카드 너비 제한 */}
+                 
+                   {/* 영상 */}
+                   <div>
+                   <div className="w-full h-full mb-1 overflow-hidden">
+                    <img
+                      src={post.thumbFilePath}
+                      alt={post.fileTitle}
+                     className="w-60 h-60 cursor-pointer object-cover object-center"
+                    />
                     </div>
+                    </div>
+
+                {/* 제목 */}
+                <div className="flex justify-between w-full">
+                    
+                <h2 className="text-xl font-bold truncate max-w-full mx-auto justify-start" title={post.fileTitle}>
+                {post.fileTitle}
+                </h2>
+                      
+                      
+                     
+                   
+                </div>
+
+                 {/* 등록일 */}
+                 <div className="">
+                <p className="text-gray-700 mb-2">{formatDate(post.regTime)}</p>
+                </div>
+             
+
+                {/* 활성화 버튼 */}
+                <div>
+                <div className="items-center text-center row mx-auto p-2">
+                  <button
+                  type="button"
+                  className="mr-2 rounded-md bg-[#6dd7e5]
+                                      px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-sky-400 
+                                       focus-visible:outline-blue-600"
+                  onClick={() => handleActivation(post.originalResourceId)}
+                >
+                  활성화
+                </button>
                   </div>
                 </div>
               </div>
-            ))
-          ) : (
-            <div className="col-span-full text-center text-gray-500">
-              게시된 파일이 없습니다.
             </div>
-          )}
-        </div>
-  
-        {/* 페이지네이션 */}
-        {filteredPosts.length > postsPerPage && (
-          <ReactPaginate
-            previousLabel={"이전"}
-            nextLabel={"다음"}
-            breakLabel={"..."}
-            pageCount={Math.ceil(filteredPosts.length / postsPerPage)}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={5}
-            onPageChange={handlePageChange}
-            containerClassName={"flex justify-center mt-4"}
-            pageClassName={"mx-1"}
-            pageLinkClassName={
-              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
-            }
-            previousClassName={"mx-1"}
-            previousLinkClassName={
-              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
-            }
-            nextClassName={"mx-1"}
-            nextLinkClassName={
-              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
-            }
-            breakClassName={"mx-1"}
-            breakLinkClassName={
-              "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
-            }
-            activeClassName={"bg-blue-500 text-white"}
-          />
-        )}
-  
-        {/* 모달창 */}
-        {isOpen && (
-          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-          onClick={closeModal}
-          >
-            <div className="relative mx-auto rounded-lg max-w-3xl w-full h-auto max-h-[80vh]"
-            onClick={(event) => {event.stopPropagation();}}
-            >
-              <video
-                src={selectedVideo}
-               alt="파일이 없습니다."
-                className="w-full max-h-screen bg-white text-center text-gray-500"
-                controls // 비디오 컨트롤러 추가
-              />
-                  {/* 닫기 버튼 */}
-            
-                  <ImCross
-             className="absolute -top-2 -right-2 text-white cursor-pointer bg-red-500 rounded-full 
-             size-6 hover:scale-110"
-             onClick={closeModal}
-            />
-  
-            </div>
+          ))
+        ) : (
+          <div className="col-span-full text-center text-gray-500">
+            게시된 파일이 없습니다.
           </div>
         )}
       </div>
-    );
-  };
+
+      {/* 페이지네이션 */}
+      {filteredPosts.length > postsPerPage && (
+        <ReactPaginate
+          previousLabel={"이전"}
+          nextLabel={"다음"}
+          breakLabel={"..."}
+          pageCount={Math.ceil(filteredPosts.length / postsPerPage)}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          containerClassName={"flex justify-center mt-4"}
+          pageClassName={"mx-1"}
+          pageLinkClassName={
+            "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+          }
+          previousClassName={"mx-1"}
+          previousLinkClassName={
+            "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+          }
+          nextClassName={"mx-1"}
+          nextLinkClassName={
+            "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+          }
+          breakClassName={"mx-1"}
+          breakLinkClassName={
+            "px-3 py-1 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200"
+          }
+          activeClassName={"bg-blue-500 text-white"}
+        />
+      )}
+
+     
+    </div>
+  );
+};
   
 export default TrashVideoFileBoard;
