e6a360546703b38c20c910c9a7c6e6d600114a3d
KangHyeonJu <90234861+KangHyeonJu@users.noreply.github.com>
2024-10-18 10:22:13 +0900

    .

--- src/ksis/pages/fileMng/VideoResourceModal.jsx
+++ src/ksis/pages/fileMng/VideoResourceModal.jsx
@@ -1,24 +1,24 @@
 import React, { useCallback, useEffect, useState } from "react"; // React의 훅을 가져옵니다.
 import { Dialog } from "../../css/dialog"; // Dialog 컴포넌트를 가져옵니다.
 import { ImCross } from "react-icons/im";
-import { VIDEO_ORIGINAL_BASIC  } from "../../../constants/api_constant"; // 상수를 가져옵니다.
-import { format, parseISO } from 'date-fns';
+import { VIDEO_ORIGINAL_BASIC } from "../../../constants/api_constant"; // 상수를 가져옵니다.
+import { format, parseISO } from "date-fns";
 import fetcher from "../../../fetcher";
 
-const VideoResourceModal = ({ isOpen, onRequestClose, originalResourceId }) => {  
+const VideoResourceModal = ({ isOpen, onRequestClose, originalResourceId }) => {
   const [modals, setModal] = useState([]); // 모달을 관리하기 위한 상태를 선언합니다.
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태를 추가합니다.
   const itemsPerPage = 5; // 한 페이지에 5개의 항목을 보여줍니다.
 
   const loadModal = useCallback(async () => {
     fetcher
-    .get(VIDEO_ORIGINAL_BASIC + `/${originalResourceId}`)
-    .then(response => {
-      setModal(response.data); // 영상을 상태에 저장합니다.
-    })
-    .catch(error => {
-      console.error('Error fetching:', error); // 에러 발생 시 콘솔에 출력합니다.
-    });
+      .get(VIDEO_ORIGINAL_BASIC + `/${originalResourceId}`)
+      .then((response) => {
+        setModal(response.data); // 영상을 상태에 저장합니다.
+      })
+      .catch((error) => {
+        console.error("Error fetching:", error); // 에러 발생 시 콘솔에 출력합니다.
+      });
   }, [originalResourceId]); // originalResourceId가 변경될 때마다 함수를 재생성합니다.
 
   useEffect(() => {
@@ -30,45 +30,46 @@ const VideoResourceModal = ({ isOpen, onRequestClose, originalResourceId }) => {
 
   const formatDate = (dateString) => {
     try {
-        const date = parseISO(dateString);
-        return format(date, "yyyy-MM-dd");
+      const date = parseISO(dateString);
+      return format(date, "yyyy-MM-dd");
     } catch (error) {
-        console.error('Invalid date format:', dateString);
-        return 'Invalid date';
-    }}
+      console.error("Invalid date format:", dateString);
+      return "Invalid date";
+    }
+  };
 
- 
-    // 페이지네이션을 위한 변수
-    const lastIndex = currentPage * itemsPerPage;
-    const firstIndex = lastIndex - itemsPerPage;
-    const currentItems = modals.slice(firstIndex, lastIndex);
-    const totalPages = Math.ceil(modals.length / itemsPerPage);
+  // 페이지네이션을 위한 변수
+  const lastIndex = currentPage * itemsPerPage;
+  const firstIndex = lastIndex - itemsPerPage;
+  const currentItems = modals.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(modals.length / itemsPerPage);
 
-    // 페이지 이동 함수
-    const goToNextPage = () => {
-      if (currentPage < totalPages) {
-        setCurrentPage(currentPage + 1);
-      }
-    };
+  // 페이지 이동 함수
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-    const goToPreviousPage = () => {
-      if (currentPage > 1) {
-        setCurrentPage(currentPage - 1);
-      }
-    };
-
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
-
-    <Dialog open={isOpen} onClose={onRequestClose}> {/* 모달이 열렸을 때 렌더링됩니다. */}
-    <div className="items-center text-center"
-        onClick={onRequestClose}>
-      <div className="fixed inset-0 flex items-center justify-center">
-        {/* 네모틀 */}
-        <div className="relative mx-auto rounded-lg max-w-3xl w-full h-auto max-h-[80vh] p-6 bg-[#ffe69c]"
-        onClick={(event) => {event.stopPropagation();}}
-        >
-             
+    <Dialog open={isOpen} onClose={onRequestClose}>
+      {" "}
+      {/* 모달이 열렸을 때 렌더링됩니다. */}
+      <div className="items-center text-center" onClick={onRequestClose}>
+        <div className="fixed inset-0 flex items-center justify-center">
+          {/* 네모틀 */}
+          <div
+            className="relative mx-auto rounded-lg max-w-3xl w-full h-auto max-h-[80vh] p-6 bg-[#ffe69c]"
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
             {/* 닫기 버튼 */}
             <ImCross
               className="absolute -top-2 -right-2 text-white cursor-pointer bg-red-500 rounded-full size-6 hover:scale-110"
@@ -78,77 +79,104 @@ const VideoResourceModal = ({ isOpen, onRequestClose, originalResourceId }) => {
             {/* 모달 내용 */}
 
             {modals.length > 0 ? (
-
-            <div className="text-center items-center p-2">
-              {/* 첫 번째 이미지만 렌더링 */}
-              {modals.length > 0 && (
-                 <div className="">
-                  {/* 영상 */}
-                  <div className="w-full h-full overflow-hidden mx-auto my-2">
-                  <video 
-                    src={modals[0].filePath} 
-                    alt={modals[0].fileTitle} 
-                    className="w-full h-full" 
-                    controls  // 비디오 컨트롤러 추가
-                    />
+              <div className="text-center items-center p-2">
+                {/* 첫 번째 이미지만 렌더링 */}
+                {modals.length > 0 && (
+                  <div className="">
+                    {/* 영상 */}
+                    <div className="w-full h-full overflow-hidden mx-auto my-2">
+                      <video
+                        src={modals[0].filePath}
+                        alt={modals[0].fileTitle}
+                        className="w-full h-full"
+                        controls // 비디오 컨트롤러 추가
+                      />
                     </div>
                   </div>
                 )}
 
-                  {/* 테이블: 현재 페이지에 해당하는 항목만 렌더링 */}
-                 <table className="min-w-full divide-y divide-black border-collapse 
-                    border border-black mb-2 text-center">
-                    <thead>
-                      <tr className="font-bold">
-                        <td className="p-2 border border-black bg-[#ffb247]">제목</td>
-                        <td className="p-2 border border-black bg-[#ffb247]">인코딩 일자</td>
-                        <td className="p-2 border border-black bg-[#ffb247]">해상도</td>
-                        <td className="p-2 border border-black bg-[#ffb247]">포맷</td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {currentItems.map((post, index) => (
+                {/* 테이블: 현재 페이지에 해당하는 항목만 렌더링 */}
+                <table
+                  className="min-w-full divide-y divide-black border-collapse 
+                    border border-black mb-2 text-center"
+                >
+                  <thead>
+                    <tr className="font-bold">
+                      <td className="p-2 border border-black bg-[#ffb247]">
+                        제목
+                      </td>
+                      <td className="p-2 border border-black bg-[#ffb247]">
+                        인코딩 일자
+                      </td>
+                      <td className="p-2 border border-black bg-[#ffb247]">
+                        해상도
+                      </td>
+                      <td className="p-2 border border-black bg-[#ffb247]">
+                        포맷
+                      </td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {currentItems.map((post, index) =>
+                      post.fileTitle !== null ? (
                         <tr key={index}>
-                         <td
+                          <td
                             className="p-2 border border-black bg-white overflow-hidden text-ellipsis whitespace-nowrap"
                             title={post.fileTitle}
-                            style={{ maxWidth: '150px' }} // 제목 셀의 최대 너비 설정
+                            style={{ maxWidth: "150px" }} // 제목 셀의 최대 너비 설정
                           >
                             {post.fileTitle}
                           </td>
-                          <td className="p-2 border border-black bg-white">{formatDate(post.regTime)}</td>
-                          <td className="p-2 border border-black bg-white">{post.resolution}</td>
-                          <td className="p-2 border border-black bg-white">{post.format}</td>
+                          <td className="p-2 border border-black bg-white">
+                            {formatDate(post.regTime)}
+                          </td>
+                          <td className="p-2 border border-black bg-white">
+                            {post.resolution}
+                          </td>
+                          <td className="p-2 border border-black bg-white">
+                            {post.format}
+                          </td>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                      ) : null
+                    )}
+                  </tbody>
+                </table>
 
                 {/* 페이지네이션 버튼 (totalPages가 1보다 클 때만 렌더링) */}
                 {modals.length > 5 && (
-                <div className="flex justify-between mt-4">
-                  <button 
-                    className={`px-4 py-2 rounded-full border-2 
-                      ${currentPage === 1 ? 'bg-gray-300 text-gray-500 border-gray-300' : 'bg-[#ffb247] text-white border-[#ffb247] hover:bg-[#ff8812]'}
+                  <div className="flex justify-between mt-4">
+                    <button
+                      className={`px-4 py-2 rounded-full border-2 
+                      ${
+                        currentPage === 1
+                          ? "bg-gray-300 text-gray-500 border-gray-300"
+                          : "bg-[#ffb247] text-white border-[#ffb247] hover:bg-[#ff8812]"
+                      }
                     `}
-                    onClick={goToPreviousPage}
-                    disabled={currentPage === 1}
-                  >
-                    이전
-                  </button>
-                  <span className="text-xl font-bold">{currentPage} / {totalPages}</span>
-                  <button 
-                    className={`px-4 py-2 rounded-full border-2 
-                      ${currentPage === totalPages ? 'bg-gray-300 text-gray-500 border-gray-300' : 'bg-[#ffb247] text-white border-[#ffb247] hover:bg-[#ff8812]'}
+                      onClick={goToPreviousPage}
+                      disabled={currentPage === 1}
+                    >
+                      이전
+                    </button>
+                    <span className="text-xl font-bold">
+                      {currentPage} / {totalPages}
+                    </span>
+                    <button
+                      className={`px-4 py-2 rounded-full border-2 
+                      ${
+                        currentPage === totalPages
+                          ? "bg-gray-300 text-gray-500 border-gray-300"
+                          : "bg-[#ffb247] text-white border-[#ffb247] hover:bg-[#ff8812]"
+                      }
                     `}
-                    onClick={goToNextPage}
-                    disabled={currentPage === totalPages}
-                  >
-                    다음
-                  </button>
-                </div>
-              )}
-            </div>
+                      onClick={goToNextPage}
+                      disabled={currentPage === totalPages}
+                    >
+                      다음
+                    </button>
+                  </div>
+                )}
+              </div>
             ) : (
               <div className="col-span-full text-center text-gray-500">
                 파일이 없습니다.
@@ -158,7 +186,6 @@ const VideoResourceModal = ({ isOpen, onRequestClose, originalResourceId }) => {
         </div>
       </div>
     </Dialog>
-    
   );
 };
 
