bea7cea2ff898a320c685f5f54df8a9cfad711bb
jwshin <154314097+jwsh1n@users.noreply.github.com>
2024-10-28 14:33:41 +0900

    Update ImageResourceModal.jsx

--- src/ksis/pages/fileMng/ImageResourceModal.jsx
+++ src/ksis/pages/fileMng/ImageResourceModal.jsx
@@ -1,32 +1,31 @@
-import React, { useCallback, useEffect, useState } from "react"; // React의 훅을 가져옵니다.
-import { Dialog } from "../../css/dialog"; // Dialog 컴포넌트를 가져옵니다.
+import React, { useCallback, useEffect, useState } from "react";
+import { Dialog } from "../../css/dialog";
 import { ImCross } from "react-icons/im";
-import { IMG_ORIGINAL_BASIC } from "../../../constants/api_constant"; // 상수를 가져옵니다.
+import { IMG_ORIGINAL_BASIC } from "../../../constants/api_constant";
 import { format, parseISO } from "date-fns";
 import fetcher from "../../../fetcher";
 
 const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {
-  const [modals, setModal] = useState([]); // 모달을 관리하기 위한 상태를 선언합니다.
-  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태를 추가합니다.
-  const itemsPerPage = 5; // 한 페이지에 5개의 항목을 보여줍니다.
+  const [modals, setModal] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
 
   const loadModal = useCallback(async () => {
     fetcher
       .get(IMG_ORIGINAL_BASIC + `/${originalResourceId}`)
       .then((response) => {
-        setModal(response.data); // 이미지를 상태에 저장합니다.
+        setModal(response.data);
       })
       .catch((error) => {
-        console.error("fetching Error :", error); // 에러 발생 시 콘솔에 출력합니다.
+        console.error("fetching Error :", error);
       });
-  }, [originalResourceId]); // originalResourceId가 변경될 때마다 함수를 재생성합니다.
+  }, [originalResourceId]);
 
   useEffect(() => {
-    // 모달이 열리고 originalResourceId가 있을 때 데이터를 로드합니다.
     if (isOpen && originalResourceId) {
       loadModal();
     }
-  }, [isOpen, originalResourceId, loadModal]); // 의존성 배열에 따라 실행됩니다.
+  }, [isOpen, originalResourceId, loadModal]);
 
   const formatDate = (dateString) => {
     try {
@@ -38,13 +37,11 @@ const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {
     }
   };
 
-  // 페이지네이션을 위한 변수
   const lastIndex = currentPage * itemsPerPage;
   const firstIndex = lastIndex - itemsPerPage;
   const currentItems = modals.slice(firstIndex, lastIndex);
   const totalPages = Math.ceil(modals.length / itemsPerPage);
 
-  // 페이지 이동 함수
   const goToNextPage = () => {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
@@ -59,83 +56,77 @@ const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {
 
   return (
     <Dialog open={isOpen} onClose={onRequestClose}>
-      {" "}
-      {/* 모달이 열렸을 때 렌더링됩니다. */}
       <div className="items-center text-center" onClick={onRequestClose}>
         <div className="fixed inset-0 flex items-center justify-center">
-          {/* 네모틀 */}
+        <div
+            className="relative mx-auto rounded-lg w-full h-auto max-w-3xl max-h-[80vh] p-6 bg-white"
+          
+          >
+        <ImCross
+              className="absolute -top-2 -right-2 text-white cursor-pointer bg-red-500 rounded-full size-6 hover:scale-125 "
+              onClick={onRequestClose}
+            />
           <div
-            className="relative mx-auto rounded-lg w-full h-auto max-w-3xl max-h-[80vh] p-6 bg-white border-gray-400"
+            className="relative mx-auto rounded-lg w-full h-auto max-w-3xl max-h-[80vh] p-6 bg-white border-gray-400 overflow-hidden"
             onClick={(event) => {
               event.stopPropagation();
             }}
           >
-            {/* 닫기 버튼 */}
-            <ImCross
-              className="absolute -top-2 -right-2 text-white cursor-pointer bg-red-500 rounded-full size-6 hover:scale-125 "
-              onClick={onRequestClose}
-            />
+            
 
-            {/* 모달 내용 */}
             {modals.length > 0 ? (
-              <div className="text-center items-center p-2">
-                {/* 첫 번째 이미지만 렌더링 */}
+              <div className="text-center items-center p-2 max-h-[70vh] overflow-y-auto">
                 {modals.length > 0 && (
-                  <div className="w-full h-full max-w-lg max-h-[80vh] mx-auto">
-                    {/* 이미지 */}
-                    <div className="w-full h-full overflow-hidden mx-auto my-2">
-                      <img
-                        src={modals[0].filePath}
-                        alt={modals[0].fileTitle}
-                        className="w-full h-full"
-                      />
-                    </div>
+                  <div className="w-full h-auto max-w-lg mx-auto my-2">
+                    <img
+                      src={modals[0].filePath}
+                      alt={modals[0].fileTitle}
+                      className="w-full h-auto max-h-[40vh] object-contain"
+                    />
                   </div>
                 )}
 
-                 {/* 테이블: 현재 페이지에 해당하는 항목만 렌더링 */}
-                 {currentItems.some(post => post.encodedResourceId !== null) ? ( // 현재 아이템이 있을 때만 테이블 렌더링
-                   <table className="w-full table-fixed border-collapse mt-4">
-                <thead className="border-t border-b border-double border-[#FF9C00]">
-                        <tr className="font-bold">
-                          <th className="w-5/12 p-2">제목</th>
-                          <th className="w-3/12 p-2">인코딩 일자</th>
-                          <th className="w-3/12 p-2">해상도</th>
-                          <th className="w-2/12 p-2"> 포맷</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {currentItems.map((post, index) =>
-                          post.fileTitle !== null ? (
-                            <tr key={index}>
-                              <td
-                                className="text-left p-2 border-b border-gray-300 bg-white overflow-hidden text-ellipsis whitespace-nowrap"
-                                title={post.fileTitle}
-                                style={{ maxWidth: "150px" }} // 제목 셀의 최대 너비 설정
-                              >
-                                {post.fileTitle}
-                              </td>
-                              <td className="p-2 text-gray-800 text-center border-b border-gray-300">
-                                {formatDate(post.regTime)}
-                              </td>
-                              <td className="p-2 text-gray-800 text-center border-b border-gray-300">
-                                {post.resolution}
-                              </td>
-                              <td className="p-2 text-gray-800 text-center border-b border-gray-300">
-                                {post.format}
-                              </td>
-                            </tr>
-                          ) : null
-                        )}
-                      </tbody>
-                    </table>
-                  ) : (
-                    <div className="col-span-full text-center text-gray-500">
-                      인코딩된 파일이 없습니다.
-                    </div>
-                  )}
+                {currentItems.some(post => post.encodedResourceId !== null) ? (
+                  <table className="w-full table-fixed border-collapse mt-4">
+                    <thead className="border-t border-b border-double border-[#FF9C00]">
+                      <tr className="font-bold">
+                        <th className="w-5/12 p-2">제목</th>
+                        <th className="w-3/12 p-2">인코딩 일자</th>
+                        <th className="w-3/12 p-2">해상도</th>
+                        <th className="w-2/12 p-2"> 포맷</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {currentItems.map((post, index) =>
+                        post.fileTitle !== null ? (
+                          <tr key={index}>
+                            <td
+                              className="text-left p-2 border-b border-gray-300 bg-white overflow-hidden text-ellipsis whitespace-nowrap"
+                              title={post.fileTitle}
+                              style={{ maxWidth: "150px" }}
+                            >
+                              {post.fileTitle}
+                            </td>
+                            <td className="p-2 text-gray-800 text-center border-b border-gray-300">
+                              {formatDate(post.regTime)}
+                            </td>
+                            <td className="p-2 text-gray-800 text-center border-b border-gray-300">
+                              {post.resolution}
+                            </td>
+                            <td className="p-2 text-gray-800 text-center border-b border-gray-300">
+                              {post.format}
+                            </td>
+                          </tr>
+                        ) : null
+                      )}
+                    </tbody>
+                  </table>
+                ) : (
+                  <div className="col-span-full text-center text-gray-500">
+                    인코딩된 파일이 없습니다.
+                  </div>
+                )}
 
-                {/* 페이지네이션 버튼 (totalPages가 1보다 클 때만 렌더링) */}
                 {modals.length > 5 && (
                   <div className="flex justify-between mt-4">
                     <button
@@ -177,6 +168,7 @@ const FileBoardModal = ({ isOpen, onRequestClose, originalResourceId }) => {
             )}
           </div>
         </div>
+      </div>  
       </div>
     </Dialog>
   );
